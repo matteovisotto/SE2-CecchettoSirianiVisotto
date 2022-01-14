9b0a5cddacdc053dddd953c3402dbe53fe5575d0
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-01-14 14:34:31 +0100

    Add api role manager

--- ForumApp/src/main/java/org/dream/forum/api/UserResource.java
+++ ForumApp/src/main/java/org/dream/forum/api/UserResource.java
@@ -1,14 +1,17 @@
 package org.dream.forum.api;
 
-import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 
 @Path("/user")
 public class UserResource {
+    @Context
+    HttpServletRequest request;
 
     @GET
     @Produces("application/json")
@@ -33,7 +36,13 @@ public class UserResource {
         return "User with mail " + mail;
     }
 
-
+    @GET
+    @Path("/wstoken")
+    @Produces("application/json")
+    @RolesAllowed({"user", "policy_maker", "admin"})
+    public String getWebServerToken() {
+        return "";
+    }
 
 
 
