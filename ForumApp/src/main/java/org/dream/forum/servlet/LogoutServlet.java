d0643b967596035739292697f2bcbfe83b18ec32
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-01-15 21:02:23 +0100

    Add Shibboleth logout redirect

--- ForumApp/src/main/java/org/dream/forum/servlet/LogoutServlet.java
+++ ForumApp/src/main/java/org/dream/forum/servlet/LogoutServlet.java
@@ -16,7 +16,7 @@ public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
         session.invalidate();
-        resp.sendRedirect(req.getContextPath()+"/");
+        resp.sendRedirect("/Shibboleth.sso/Logout?return="+req.getScheme()+"://"+req.getServerName()+((req.getServerPort()==80 || req.getServerPort()==443) ? "" : ":"+req.getServerPort())+req.getContextPath());
     }
 
     @Override
