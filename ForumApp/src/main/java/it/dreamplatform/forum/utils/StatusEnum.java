bfc6631cd982ad8f135bf70e450846a57a972959
TiaSirio <10571322@polimi.it>
2022-01-22 16:58:50 +0100

    Added function to retrieve Status.

--- ForumApp/src/main/java/it/dreamplatform/forum/utils/StatusEnum.java
+++ ForumApp/src/main/java/it/dreamplatform/forum/utils/StatusEnum.java
@@ -1,5 +1,16 @@
 package it.dreamplatform.forum.utils;
 
 public enum StatusEnum {
-    PENDING,ACCEPTED;
+    PENDING,ACCEPTED,ERROR;
+
+    StatusEnum getStatusById(int statusId) {
+        switch (statusId) {
+            case 0:
+                return StatusEnum.PENDING;
+            case 1:
+                return StatusEnum.ACCEPTED;
+            default:
+                return StatusEnum.ERROR;
+        }
+    }
 }
