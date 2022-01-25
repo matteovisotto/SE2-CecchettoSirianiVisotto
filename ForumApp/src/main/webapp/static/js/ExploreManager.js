0c11346dd0e767ba2b138194fb5400bac81f96a7
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-01-25 18:30:29 +0100

    Fix explore

--- ForumApp/src/main/webapp/static/js/ExploreManager.js
+++ ForumApp/src/main/webapp/static/js/ExploreManager.js
@@ -1,20 +1,16 @@
 $(function () {
 
     var discussionContainer = $('#discussionContainer');
+    var topics = [];
     //var topicTitle = $('#topicTitle');
+
     $.ajax(
         {
-            'url': 'api/discussion/explore',
+            'url': 'api/topic',
             'method': 'GET',
             'success': function(json){
-                //topicTitle.text(json.title);
-                if (json.discussions.length === 0) {
-                    createEmptyTopicAlert();
-                } else {
-                    json.discussions.forEach(d => {
-                        createDiscussionNode(d, json);
-                    });
-                }
+                topics = json;
+                loadData();
             },
             'error':function(){
                 alert('Invalid discussions');
@@ -23,23 +19,45 @@ $(function () {
     );
 
 
+    function loadData() {
+        $.ajax(
+            {
+                'url': 'api/discussion/explore',
+                'method': 'GET',
+                'success': function (json) {
+                    if (json.length === 0) {
+                        createEmptyTopicAlert();
+                    } else {
+                        json.forEach(d => {
+                            createDiscussionNode(d);
+                        });
+                    }
+                },
+                'error': function () {
+                    alert('Invalid discussions');
+                }
+            }
+        );
+
+    }
+
     function createEmptyTopicAlert(){
         var alert = $('<div/>');
         alert.addClass('alert alert-primary').text("No discussions available");
         discussionContainer.append(alert);
     }
 
-    function createDiscussionNode(d, t) {
+    function createDiscussionNode(d) {
         var className = 'vstack gap-0 text-secondary mx-auto align-items-center';
         var rank = $('<div/>').addClass(className).append($('<i/>').addClass('fas fa-sort-up fa-2x text-secondary')).append($('<strong/>').text(d.number_replies)).append($('<i/>').addClass('fas fa-sort-down fa-2x text-secondary'));
 
         var rankNode = $('<div/>').addClass('col-1 d-flex').append(rank);
 
-        var topicNode = $('<span/>').addClass('text-secondary float-end small').text("Topic: " + t.title);
+        var topicNode = $('<span/>').addClass('text-secondary float-end small').text("Topic: " + getTopicNameByID(d.topicId));
         var h4Node = $('<h4/>').addClass('card-title').text(d.title);
-        var descriptionNode = $('<div/>').addClass('card-text').text(d.text);
+        var descriptionNode = $('<div/>').addClass('card-text').html(d.text);
         var authorNode = '<table class="table table-borderless text-secondary"><tbody><tr class="small">' +
-            '<td><img src="../static/assets/placeholder_user.png" class="top-user-img"> Posted' +
+            '<td><img src="static/assets/placeholder_user.png" class="top-user-img"> Posted' +
             'by: <strong>'+d.creator.name + ' ' + d.creator.surname+'</strong></td>' +
             '<td>' + d.timestamp + '</td>' +
             '<td class="text-sm-end"><i class="fas fa-comments"></i> '+ d.number_replies +'</td>' +
@@ -48,13 +66,23 @@ $(function () {
 
         var card = $('<div/>').addClass('card border-0 shadow dream-card').append($('<div/>').addClass('card-body').append($('<div/>').addClass('row').append(rankNode).append(textContainer)));
 
-        var aNode = $('<a/>').addClass('link-card').attr('href', '../discussion/'+d.discussionId).append(card);
+        var aNode = $('<a/>').addClass('link-card').attr('href', 'discussion/'+d.discussionId).append(card);
 
         var container = $('<div/>').addClass('col-12 card-container').append(aNode);
 
         discussionContainer.append(container);
 
 
+    }
+
+
+    function getTopicNameByID(tId) {
+        topics.forEach(t => {
+            if(t.topicId === tId){
+                return t.title;
+            }
+        });
+        return "";
     }
 
 });
