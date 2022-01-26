b87d63e2f3d8ee60663afcfb2533fffc71d68a8f
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-01-26 22:17:56 +0100

    Fix

--- ForumApp/src/main/webapp/static/js/ProfileManager.js
+++ ForumApp/src/main/webapp/static/js/ProfileManager.js
@@ -1,9 +1,10 @@
 $(function (){
+    //At the moment is not required
+    var profileController = new ProfileController($('#userName') );
 
-    var profileController = new ProfileController($('#userName'), )
-
-    function ProfileController(_title, ){
-
+    function ProfileController(_title){
+        var self = this;
+        this.title = _title;
     }
 
 });
