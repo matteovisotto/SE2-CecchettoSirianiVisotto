58b681a9e77381957d207ae5fb3384c0c5e1a7b9
TiaSirio <10571322@polimi.it>
2022-01-26 23:57:52 +0100

    Implemented the decline a post and starting implementation of approve a post.

--- ForumApp/src/main/webapp/static/js/ModeratorArea.js
+++ ForumApp/src/main/webapp/static/js/ModeratorArea.js
@@ -1,5 +1,53 @@
 $(function () {
 
+    var MODAL_ACTION = "update";
+    var current_selected_post = undefined;
+
+    if(typeof userId !== 'undefined'){
+        CKEDITOR.replace('replyContent');
+
+        $('#modalCreateReplyButton').on('click', function (e){
+            var form = $('#approveReplyForm');
+            if(MODAL_ACTION === 'update'){
+                if(current_selected_post !== undefined){
+                    current_selected_post.text = CKEDITOR.instances.replyContent.getData();
+                    current_selected_post.timestamp = new Date();
+                    $.ajax({
+                        type: "POST",
+                        url: "api/post/modify",
+                        data: JSON.stringify(current_selected_post),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function(data){
+                            $('#approveReplyModal').modal('hide');
+                            CKEDITOR.instances.replyContent.setData("");
+                            //$('#modalUpdateButton').text("Approve");
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Post successfully modified',
+                                showConfirmButton: false,
+                                timer: 1500
+                            }).then((r) => {
+                                //Qua andrebbe chiamata la approvePost penso
+                                loadData();
+                            });
+                        },
+                        error: function(e) {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'An error occurred. Please try again',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        }
+                    });
+                }
+            }
+
+        });
+
+    }
+
     var repliesContainer = $('#moderatorRepliesContainer');
 
     loadData();
@@ -14,6 +62,7 @@ $(function () {
                     json.forEach(d => {
                         createReplyNode(d);
                     });
+                    registerActions();
                 },
                 'error':function(){
                     alert('Invalid discussion');
@@ -22,6 +71,51 @@ $(function () {
         );
     }
 
+    function registerActions() {
+        $('.modifyDropdown').on('click', function (e){
+            MODAL_ACTION = "update";
+            var postId = $(e.target).data('post-id');
+            fetch('api/post/'+postId).then(response => {
+                return response.json();
+            }).then(json => {
+                current_selected_post = json;
+                CKEDITOR.instances.replyContent.setData(json.text);
+                $('#modalUpdateButton').text("Approve");
+                $('#approveReplyModal').modal('show');
+            });
+        });
+
+        $('.deleteDropdown').on('click', function (e){
+            var postId = $(e.target).data('post-id');
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, decline it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.post( "api/post/delete/"+postId, function( data ) {
+                        Swal.fire(
+                            'Declined!',
+                            'This post has been deleted.',
+                            'success'
+                        )
+                        loadData();
+                    }).fail(function (error){
+                        Swal.fire(
+                            'Error!',
+                            'An error occured while deleting',
+                            'error'
+                        )
+                    });
+
+                }
+            });
+        });
+    }
 
     function createReplyNode(p) {
         var image = '<img src="static/assets/placeholder_user.png" class="card-profile">';
@@ -37,6 +131,12 @@ $(function () {
                 .append(area)
         );
         var postContent = $('<div/>').addClass('col-9');
+        postContent.append('<a class="float-end text-secondary" id="cardDropdown'+p.postId+'" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></a>\n' +
+                '                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="cardDropdown'+p.postId+'" >\n' +
+                '                                        <li><a class="dropdown-item modifyDropdown" href="#" data-post-id="'+p.postId+'"><i class="fas fa-pen"></i> Modify</a></li>\n' +
+                '                                        <li><a class="dropdown-item deleteDropdown" href="#" data-post-id="'+p.postId+'"><i class="fa fa-trash"></i> Decline</a></li>\n' +
+                '                                    </ul>');
+        postContent.append($('<h6/>').addClass('text-primary').html('Discussion: ' + p.discussionId));
         postContent.append($('<div/>').addClass('card-text').html(p.text));
         var postRow = $('<div/>').addClass('row').append(userInfo).append(postContent);
         var container = $('<div/>').addClass("col-12 card-container").append(
