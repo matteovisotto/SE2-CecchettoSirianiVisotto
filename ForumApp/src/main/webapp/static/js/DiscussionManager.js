daa378bcd6401edd2358248be40eea4a32155d59
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-01-26 18:23:13 +0100

    Reformat DiscussionManager.js

--- ForumApp/src/main/webapp/static/js/DiscussionManager.js
+++ ForumApp/src/main/webapp/static/js/DiscussionManager.js
@@ -1,38 +1,59 @@
 $(function () {
 
+    var discussionController = new DiscussionController($('#topicTitle'), $('#discussionContainer'), topicId);
+    discussionController.loadData();
+
     if(typeof isPolicyMaker !== 'undefined' && isPolicyMaker){
-        CKEDITOR.replace('discussionContent');
+        var modalController = new ModalController($('#newDiscussionModal'), $('#newDiscussionForm'), $('#modalCreateDiscussionButton'), userId, topicId);
+        modalController.init();
+    }
 
-        $('#modalCreateDiscussionButton').on('click', function (e){
-            var form = $('#newDiscussionForm');
+    function ModalController(_modal, _form, _formButton, _userId, _topicId){
+        var self = this;
+        this.modal = _modal;
+        this.form = _form;
+        this.button = _formButton;
+        this.userId = _userId;
+        this.topicId = _topicId;
+
+        this.init = function (){
+            CKEDITOR.replace('discussionContent');
+            this.button.on('click', function (e){
+                self.postData();
+            });
+        }
+
+        this.getDiscussiOnbject = function() {
             var postObj = {};
-
             var discussionObj = {};
-            discussionObj.title = form.find('input[name="discussionTitle"]').val();
+            discussionObj.title = this.form.find('input[name="discussionTitle"]').val();
             var text = CKEDITOR.instances.discussionContent.getData();
             discussionObj.text = text;
             discussionObj.timestamp = new Date();
-            discussionObj.topicId = topicId;
+            discussionObj.topicId = this.topicId;
             postObj.text = text;
             postObj.timestamp = new Date();
-            postObj.creator = {"userId": userId};
+            postObj.creator = {"userId": this.userId};
             discussionObj.posts = [postObj];
+            return discussionObj;
+        }
+
+        this.postData = function (){
             $.ajax({
                 type: "POST",
                 url: "../api/discussion/create",
-                // The key needs to match your method's input parameter (case-sensitive).
-                data: JSON.stringify(discussionObj),
+                data: JSON.stringify(self.getDiscussiOnbject()),
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 success: function(data){
-                    $('#newDiscussionModal').modal('hide');
+                    self.modal.modal('hide');
                     Swal.fire({
                         icon: 'success',
                         title: 'Discussion successfully added',
                         showConfirmButton: false,
                         timer: 1500
                     }).then((r) => {
-                        loadData();
+                        discussionController.loadData();
                     });
                 },
                 error: function(e) {
@@ -41,75 +62,71 @@ $(function () {
                         title: 'An error occurred. Please try again',
                         showConfirmButton: false,
                         timer: 1500
-                    }).then((r) => {
-                        loadData();
                     });
                 }
             });
-        });
+        }
 
     }
 
-    var discussionContainer = $('#discussionContainer');
-    var topicTitle = $('#topicTitle');
 
-    loadData();
+    function DiscussionController(_title, _container, _topicId) {
+        var self = this;
+        this.title = _title;
+        this.container = _container;
+        this.topicId = _topicId;
 
-    function loadData() {
-        discussionContainer.html("");
-        $.ajax(
-            {
-                'url': '../api/topic/' + topicId,
-                'method': 'GET',
-                'success': function (json) {
-                    topicTitle.text(json.title);
-                    if (json.discussions.length === 0) {
-                        createEmptyTopicAlert();
-                    } else {
-                        json.discussions.forEach(d => {
-                            createDiscussionNode(d, json);
-                        });
-                    }
-                },
-                'error': function () {
-                    alert('Invalid topic');
+        this.loadData = function() {
+            this.container.html("");
+            $.get('../api/topic/' + this.topicId, (json) => {
+                this.title.text(json.title);
+                if (json.discussions.length === 0) {
+                    self.createEmptyAlert();
+                } else {
+                    json.discussions.forEach(d => {
+                        self.createNode(d, json);
+                    });
                 }
-            }
-        );
+            }).fail((error) => {
+                self.conteiner.append('<div class="alert alert-danger">An error occurred. Please try later</div>');
+            });
+
+        }
+
+
+        this.createEmptyAlert = function() {
+            var alert = $('<div/>');
+            alert.addClass('alert alert-primary').text("No discussions available");
+            this.container.append(alert);
+        }
+
+        this.createNode = function(d, t) {
+            var className = 'vstack gap-0 text-secondary mx-auto align-items-center';
+            var rank = $('<div/>').addClass(className).append($('<i/>').addClass('fas fa-sort-up fa-2x text-secondary')).append($('<strong/>').text(d.number_replies)).append($('<i/>').addClass('fas fa-sort-down fa-2x text-secondary'));
+
+            var rankNode = $('<div/>').addClass('col-1 d-flex').append(rank);
+
+            var topicNode = $('<span/>').addClass('text-secondary float-end small').text("Topic: " + t.title);
+            var h4Node = $('<h4/>').addClass('card-title').text(d.title);
+            var descriptionNode = $('<div/>').addClass('card-text').html(d.text);
+            var authorNode = '<table class="table table-borderless text-secondary"><tbody><tr class="small">' +
+                '<td><img src="../static/assets/placeholder_user.png" class="top-user-img"> Posted' +
+                'by: <strong>'+d.creator.name + ' ' + d.creator.surname+'</strong></td>' +
+                '<td>' + d.timestamp + '</td>' +
+                '<td class="text-sm-end"><i class="fas fa-comments"></i> '+ d.number_replies +'</td>' +
+                '</tr></tbody></table>';
+            var textContainer = $('<div/>').addClass('col-11').append(topicNode).append(h4Node).append(descriptionNode).append('<hr/>').append(authorNode);
+
+            var card = $('<div/>').addClass('card border-0 shadow dream-card').append($('<div/>').addClass('card-body').append($('<div/>').addClass('row').append(rankNode).append(textContainer)));
+
+            var aNode = $('<a/>').addClass('link-card').attr('href', '../discussion/'+d.discussionId).append(card);
+
+            var container = $('<div/>').addClass('col-12 card-container').append(aNode);
+
+            this.container.append(container);
+        }
     }
 
-    function createEmptyTopicAlert(){
-        var alert = $('<div/>');
-        alert.addClass('alert alert-primary').text("No discussions available");
-        discussionContainer.append(alert);
-    }
 
-    function createDiscussionNode(d, t) {
-        var className = 'vstack gap-0 text-secondary mx-auto align-items-center';
-        var rank = $('<div/>').addClass(className).append($('<i/>').addClass('fas fa-sort-up fa-2x text-secondary')).append($('<strong/>').text(d.number_replies)).append($('<i/>').addClass('fas fa-sort-down fa-2x text-secondary'));
-
-        var rankNode = $('<div/>').addClass('col-1 d-flex').append(rank);
-
-        var topicNode = $('<span/>').addClass('text-secondary float-end small').text("Topic: " + t.title);
-        var h4Node = $('<h4/>').addClass('card-title').text(d.title);
-        var descriptionNode = $('<div/>').addClass('card-text').html(d.text);
-        var authorNode = '<table class="table table-borderless text-secondary"><tbody><tr class="small">' +
-            '<td><img src="../static/assets/placeholder_user.png" class="top-user-img"> Posted' +
-            'by: <strong>'+d.creator.name + ' ' + d.creator.surname+'</strong></td>' +
-            '<td>' + d.timestamp + '</td>' +
-            '<td class="text-sm-end"><i class="fas fa-comments"></i> '+ d.number_replies +'</td>' +
-            '</tr></tbody></table>';
-        var textContainer = $('<div/>').addClass('col-11').append(topicNode).append(h4Node).append(descriptionNode).append('<hr/>').append(authorNode);
-
-        var card = $('<div/>').addClass('card border-0 shadow dream-card').append($('<div/>').addClass('card-body').append($('<div/>').addClass('row').append(rankNode).append(textContainer)));
-
-        var aNode = $('<a/>').addClass('link-card').attr('href', '../discussion/'+d.discussionId).append(card);
-
-        var container = $('<div/>').addClass('col-12 card-container').append(aNode);
-
-        discussionContainer.append(container);
-
-
-    }
 
 });
